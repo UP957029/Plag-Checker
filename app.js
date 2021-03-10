3e57be88b4c2e34acababdca1b42f6c53ea31b3e
Ben <72570746+UP957029@users.noreply.github.com>
2021-03-10 21:38:49 +0000

    html js code moved to app.js

--- app.js
+++ app.js
@@ -1,31 +1,76 @@
+const dropZone = document.querySelector(".drop-zone");
+const button = document.querySelector(".drop-zone__button")
+//global variable, will be used in a few functions 
+let file; 
+const dragText = document.querySelector('#dropText')
+const inputFile = document.querySelector('.drop-zone__file')
 
-applicationCache.use('/index')
+
+dropZone.addEventListener("dragover", (e)=>{
+  e.preventDefault();
+    dropZone.classList.add("drop-zone--over");
+    dragText.textContent = "Release file to uplaod"
+})
+
+dropZone.addEventListener("dragleave", (e)=>{
+  e.preventDefault();
+  dropZone.classList.remove("drop-zone--over")
+  dragText.textContent = "Drag and drop file(s) here"
+})
+
+dropZone.addEventListener("drop", (event) => {
+  event.preventDefault();
+  
+  file = event.dataTransfer.files
+  let readers = []
+  showFiles()
+  
+
+});
+
+button.addEventListener("click", ()=>{
+  inputFile.click()
+})
+
+inputFile.addEventListener("change", function(){
+  file = this.files
+  showFiles();
+  
+})
+
+document.querySelector(".drop-zone__submit").addEventListener("click", ()=>{
+  console.log(file)
+})
 
 
-document.getElementById("compareBtn").addEventListener('submit', compareFiles);
-
-
-function compareFiles(){
-   
-
+// This function will display the files text content
+function showFiles(){
+  for(let i =0; i<file.length; i++){
+  let fileType = file[i].type
+  let validExtensions = ["text/javascript"]; 
+  if(validExtensions.includes(fileType)){
+    let fileReader = new FileReader();
+    fileReader.onload = () =>{
+      let fileURL = fileReader.result;
+      console.log(fileURL)
+    }
+    fileReader.readAsDataURL(file[i])
+  }else{
+    alert("Invalid File")
+    dropZone.classList.remove("drop-zone--over")
+  }
+}
 }
 
+  //submits the uplaoded files to the server "server.js"
+  const form = document.getElementById("submitForm");
 
-function showReport(){
-let files = JSON.parse(localStorage.getItem('files'))
-let codeReport = document.getElementById('codeReport')
+  form.addEventListener("submit", e=>{
+    e.preventDefault();
 
-codeReport.textContent = '';
+    const endpoint = "upload.js";
+    const formdData = new FormData();
+    formdData.append("file", file.files)
+    //console.log(file)
 
-for (let i = 0; i , files.length;i++){
-    let name = files[i].name
-    let similarity = files[i].similarity
-    let size = files[i].size 
-
-    codeReport.textContent += '<tr>'
-                            + name + 
-                            similarity + 
-                            size +  '</tr>'
-
-}
-}
+  });
