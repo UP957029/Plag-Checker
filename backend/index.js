7706c80f0c79b4360d68cbcbfe2048543a48fca5
Ben <72570746+UP957029@users.noreply.github.com>
2021-05-31 19:20:25 +0100

    new way of uploading files

--- backend/index.js
+++ backend/index.js
@@ -2,36 +2,37 @@ const path = require('path');
 const fs = require('fs');
 const stringSimilarity = require('string-similarity');
 
+
+function compareFiles() {
 // get a list of path names
-
-const files = fs.readdirSync('Plag-Checker/uploads');
-//
-const pathArr = [];
-for (let i = 0; i < files.length; i++) {
-  const filePath = path.join(files[i]);
-  pathArr[i] = 'Plag-Checker/uploads' + '/' + filePath;
+  const files = fs.readdirSync('Plag-Checker/uploads');
+  //
+  const pathArr = [];
+  for (let i = 0; i < files.length; i++) {
+    const filePath = path.join(files[i]);
+    pathArr[i] = 'Plag-Checker/uploads' + '/' + filePath;
   // console.log(pathArr[i]);
-}
+  }
 
-// readfile sync in loop using the path array to create a list of files data in strin
-const dataArr = [];
-for (let i = 0; i < files.length; i++) {
-  const dataString = fs.readFileSync(pathArr[i], 'utf-8');
-  dataArr[i] = dataString;
-}
-
-
-const original = dataArr.shift();
-dataArr.shift();
-const targetFiles = dataArr;
+  // readfile sync in loop using the path array to create a list of files data in strin
+  const dataArr = [];
+  for (let i = 0; i < files.length; i++) {
+    const dataString = fs.readFileSync(pathArr[i], 'utf-8');
+    dataArr[i] = dataString;
+  }
 
 
-const similarity = stringSimilarity.findBestMatch(original, targetFiles);
-console.log(similarity); // Returns a fraction between 0 and 1
+  const original = dataArr.shift();
+  dataArr.shift();
+  const targetFiles = dataArr;
 
 
-// used to delete all files after
-function deleteFiles() {
+  const similarity = stringSimilarity.findBestMatch(original, targetFiles);
+  console.log(similarity); // Returns a fraction between 0 and 1
+
+
+  // used to delete all files after
+
   for (let i = 0; i < pathArr.length; i++) {
     fs.unlink(pathArr[i], (err) => {
       if (err) {
@@ -42,4 +43,5 @@ function deleteFiles() {
     });
   }
 }
-deleteFiles();
+
+compareFiles();
