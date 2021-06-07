096248054367c1fb168da665ec94f8d5f4a5c77d
Ben <72570746+UP957029@users.noreply.github.com>
2021-06-07 14:49:34 +0100

    uplaods added

---
+++ uploads/server-upload.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const multer = require('multer');
+const md5File = require('md5-file/promise');
+
+const config = require('./config.json');
+
+const app = express();
+const configuredMulter = multer(config);
+const single = configuredMulter.single('md5me');
+
+async function upload(req, res) {
+    const response = {
+        name: req.file.originalname,
+        hash: await md5File(req.file.path)
+    };
+    res.json(response);
+}
+
+app.use('/', express.static('webpages'));
+app.post('/upload', single, upload);
+
+// error handling
+app.use((error, req, res, next) => {
+    if (error.code === 'LIMIT_FILE_SIZE') {
+        res.status(413).send('Request Entity Too Large');
+    } else {
+        res.status(500).send('Server Error');
+    }
+});
+
+app.listen(8080, (e) => {
+    console.log(`server ${e ? 'failed to start' : 'listening'}`);
+});
+
+
